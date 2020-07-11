3099d9be0f32ce472b91fa5f543719c804e63809
aidanhartman <54724161+aidanhartman@users.noreply.github.com>
2020-07-11 05:19:45 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function (req, res) {
+
+})
+
+server.listen(port, function(error) {
+if (error) {
+console.log('Something went wrong', error)
+} else ('Server is listening to port' + port}
+})
